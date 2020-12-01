f9247580d4fdd5ab73fedd57be9c1604c684705c
Chris Emery <chris@menageriehouse.net>
2020-12-01 07:46:31 -0800

    Initial commit for Day01

---
+++ target/debug/day01.d
@@ -0,0 +1,1 @@
+/Users/emeryc/src/advent_of_code_2020/target/debug/day01: /Users/emeryc/src/advent_of_code_2020/day01/src/main.rs
---
+++ target/debug/deps/day01.d
@@ -0,0 +1,5 @@
+/Users/emeryc/src/advent_of_code_2020/target/debug/deps/day01: day01/src/main.rs
+
+/Users/emeryc/src/advent_of_code_2020/target/debug/deps/day01.d: day01/src/main.rs
+
+day01/src/main.rs:
