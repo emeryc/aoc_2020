e53ca5aa140bed6fd0ca7ea2e1163462447cec02
Chris Emery <chris@menageriehouse.net>
2020-12-01 07:50:59 -0800

    whoops target

--- target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/Users/emeryc/src/advent_of_code_2020/target/debug/day01: /Users/emeryc/src/advent_of_code_2020/day01/src/main.rs
--- target/debug/deps/day01.d
+++
@@ -1,5 +0,0 @@
-/Users/emeryc/src/advent_of_code_2020/target/debug/deps/day01: day01/src/main.rs
-
-/Users/emeryc/src/advent_of_code_2020/target/debug/deps/day01.d: day01/src/main.rs
-
-day01/src/main.rs:
