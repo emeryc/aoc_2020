f5b4c68cdf3175de0419919c247c178d20a91178
chris <chris@98point6.com>
2020-12-01 16:30:33 -0800

    Better Tripple, now dealing with failures

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use anyhow::Result;
 use std::io::prelude::*;
 use std::{fs::File, path::PathBuf};
@@ -21,77 +22,76 @@ fn main() -> Result<()> {
         .filter_map(|s| s.parse().ok())
         .collect::<Vec<i32>>();
 
-    let pair = find_pair(i_list.as_slice());
+    let pair = find_pair(i_list.as_slice(), 2020).ok_or_else(|| anyhow!("No pair found"))?;
 
     println!("{}", pair.iter().product::<i32>());
 
-    let tripple = find_tripple(i_list.as_slice());
+    let tripple = find_tripple(i_list.as_slice()).ok_or_else(|| anyhow!("No tripple found"))?;
 
     println!("{}", tripple.iter().product::<i32>());
 
     Ok(())
 }
 
-fn find_pair(list: &[i32]) -> [i32; 2] {
+fn find_pair(list: &[i32], target: i32) -> Option<[i32; 2]> {
     let mut list = Vec::from(list);
-    list.sort();
+    list.sort_unstable();
     let mut head = 0;
-    let mut tail = list.len() / 2;
+    let mut tail = list.len() - 1;
     loop {
-        let sum = dbg!(list.get(head).expect("Should work") + list.get(tail).expect("should work"));
+        if head >= tail {
+            return None;
+        }
+        let sum = list.get(head).expect("Should work") + list.get(tail).expect("should work");
         match sum {
-            sum if sum > 2020 => {
+            sum if sum > target => {
                 tail -= 1;
             }
-            sum if sum < 2020 => {
+            sum if sum < target => {
                 tail += 1;
                 head += 1;
             }
             _ => {
-                break [
+                break Some([
                     *list.get(head).expect("already got this"),
                     *list.get(tail).expect("already got this"),
-                ];
+                ]);
             }
         }
     }
 }
 
-fn find_tripple(list: &[i32]) -> [i32; 3] {
+fn find_tripple(list: &[i32]) -> Option<[i32; 3]> {
     for i in 0..(list.len() - 2) {
-        for j in i..(list.len() - 1) {
-            for k in j..list.len() {
-                if list.get(i).unwrap() + list.get(j).unwrap() + list.get(k).unwrap() == 2020 {
-                    return [
-                        *list.get(i).unwrap(),
-                        *list.get(j).unwrap(),
-                        *list.get(k).unwrap(),
-                    ];
-                }
-            }
+        if let Some(p) = find_pair(&list[i..], 2020 - list.get(i)?) {
+            return Some([*list.get(i)?, p[0], p[1]]);
         }
     }
-    unreachable!();
+    None
 }
 
 #[cfg(test)]
 mod test {
+    use anyhow::{anyhow, Result};
+
     use crate::{find_pair, find_tripple};
 
     #[test]
-    fn basic() {
-        let mut pair = find_pair(vec![1721, 979, 366, 299, 675, 1456].as_slice());
-        pair.sort();
+    fn basic() -> Result<()> {
+        let mut pair = find_pair(vec![1721, 979, 366, 299, 675, 1456].as_slice(), 2020)
+            .ok_or_else(|| anyhow!("No Pair"))?;
+        pair.sort_unstable();
         assert_eq!(pair[0], 299);
         assert_eq!(pair[1], 1721);
+        Ok(())
     }
 
     #[test]
-    fn tripple() {
-        let tripple = dbg!(find_tripple(
-            vec![1721, 979, 366, 299, 675, 1456].as_slice()
-        ));
+    fn tripple() -> Result<()> {
+        let tripple = find_tripple(vec![1721, 979, 366, 299, 675, 1456].as_slice())
+            .ok_or_else(|| anyhow!("No Tripple"))?;
         let mult: i32 = Vec::from(tripple).iter().product();
         assert_eq!(mult, 241861950);
+        Ok(())
     }
 }
