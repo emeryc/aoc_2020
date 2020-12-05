e8054a7cd51d6265cdc6b990bdba5b0d7f4be38d
Chris Emery <chris@menageriehouse.net>
2020-12-05 15:10:22 -0800

    day5 switch to SeatId for 60% speedup

--- src/day05.rs
+++ src/day05.rs
@@ -1,106 +1,34 @@
-use anyhow::{anyhow, Error, Result};
-use std::fmt::Display;
+use anyhow::{Error, Result};
 use std::str::FromStr;
 
 #[aoc_generator(day5)]
-fn generator(input: &str) -> Result<Vec<BoardingPass>> {
+fn generator(input: &str) -> Result<Vec<u64>> {
     input
         .split('\n')
-        .map(|pass| pass.parse::<BoardingPass>())
+        .map(|pass| Ok(pass.parse::<SeatID>()?.0))
         .collect()
 }
 
 #[aoc(day5, part1)]
-fn solve_part1(passes: &[BoardingPass]) -> BoardingPass {
-    passes.iter().max_by_key(|p| p.id()).unwrap().clone()
+fn solve_part1(passes: &[u64]) -> u64 {
+    *passes.iter().max().unwrap()
 }
 
 #[aoc(day5, part2)]
-fn solve_part2(passes: &[BoardingPass]) -> BoardingPass {
+fn solve_part2(passes: &[u64]) -> u64 {
     let mut passes: Vec<_> = passes.to_vec();
-    passes.sort_unstable_by_key(|pass| (pass.row, pass.column));
-    let before_missing = passes
+    passes.sort_unstable();
+    passes
         .iter()
         .skip(1)
-        .fold(passes.first().unwrap(), |last, pass| {
-            let ordered = if last.column == 7 {
-                last.row + 1 == pass.row && pass.column == 0
-            } else {
-                last.row == pass.row && last.column + 1 == pass.column
-            };
-            if ordered {
-                pass
-            } else {
+        .fold(passes.first().unwrap(), |last, cur| {
+            if last + 1 != *cur {
                 last
-            }
-        });
-
-    BoardingPass {
-        row: {
-            if before_missing.column == 7 {
-                before_missing.row + 1
             } else {
-                before_missing.row
+                cur
             }
-        },
-        column: {
-            if before_missing.column == 7 {
-                0
-            } else {
-                before_missing.column + 1
-            }
-        },
-    }
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct BoardingPass {
-    row: u8,
-    column: u8,
-}
-
-impl Display for BoardingPass {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(
-            format!(
-                "row: {}, column: {}: id: {}",
-                self.row,
-                self.column,
-                self.id(),
-            )
-            .as_str(),
-        )
-    }
-}
-
-impl BoardingPass {
-    fn id(&self) -> u64 {
-        self.row as u64 * 8 + self.column as u64
-    }
-}
-
-impl FromStr for BoardingPass {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.is_empty() {
-            return Err(anyhow!("Nothing to parse"));
-        }
-        let (r, c) = s.split_at(s.len() - 3);
-
-        let (_, row) = r.chars().fold((0, 127), |(b, t), side| match side {
-            'F' => (b, (b + t) / 2),
-            'B' => ((b + t) / 2, t),
-            _ => unreachable!(),
-        });
-        let (_, column) = c.chars().fold((0, 7), |(l, r), side| match side {
-            'R' => ((l + r) / 2, r),
-            'L' => (l, (l + r) / 2),
-            _ => unreachable!(),
-        });
-
-        Ok(BoardingPass { row, column })
-    }
+        })
+        .to_owned()
 }
 
 struct SeatID(u64);
@@ -128,40 +56,7 @@ mod test {
 
     #[test]
     fn sample_0() -> Result<()> {
-        let p1 = "FBFBBFFRLR".parse::<BoardingPass>()?;
-        assert_eq!(p1, BoardingPass { row: 44, column: 5 });
-        assert_eq!(p1.id(), 357);
         assert_eq!("FBFBBFFRLR".parse::<SeatID>()?.0, 357);
-
-        Ok(())
-    }
-
-    #[test]
-    fn sample_1() -> Result<()> {
-        let p1 = "BFFFBBFRRR".parse::<BoardingPass>()?;
-        assert_eq!(p1, BoardingPass { row: 70, column: 7 });
-
-        Ok(())
-    }
-
-    #[test]
-    fn sample_2() -> Result<()> {
-        let p1 = "FFFBBBFRRR".parse::<BoardingPass>()?;
-        assert_eq!(p1, BoardingPass { row: 14, column: 7 });
-
-        Ok(())
-    }
-
-    #[test]
-    fn sample_3() -> Result<()> {
-        let p1 = "BBFFBBFRLL".parse::<BoardingPass>()?;
-        assert_eq!(
-            p1,
-            BoardingPass {
-                row: 102,
-                column: 4
-            }
-        );
 
         Ok(())
     }
